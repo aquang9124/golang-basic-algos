de8f1d046bf8fd1e0b769766c63813dca9a299bc
Alex Quang <alexquang1991@gmail.com>
2016-03-19 20:45:13 -0700

    first commit

---
+++ algos.go
@@ -0,0 +1,87 @@
+package main
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("The sum of all multiples of 3 or 5 below 1000:", threeOrFive(1000))
+	fmt.Println("evenFibonacci function output:", evenFibonacci(12))
+	fmt.Println("The factorial of 5 is:", factorial(5))
+	fmt.Println("The reverse of the string 'dlrow olleh' is:", reverseStr("dlrow olleh"))
+}
+
+// This function will find and return the sum of all those
+// integers which are multiples of 3 or 5 below x.
+func threeOrFive(x int) int {
+	// sum is initialized to 0 using short declaration
+	sum := 0
+
+	for i := 0; i < x; i++ {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i
+		}
+	}
+	// At the end return sum
+	return sum
+}
+
+/* -------------------- evenFibonacci --------------------------
+   Currently this function will generate a fibonacci sequence
+   that contains 'x' extra numbers past the original 0, 1 and
+   it will then sum up all of the numbers that are even.
+   It will break out of the initial for loop if at any point
+   the values in the fibonacci sequence become greater than
+   4 million 
+------------------------------------------------------------- */
+func evenFibonacci(x int) int {
+	// A slice called k is created with a length of 2.
+	k := make([]int, 2)
+	// Since k is initialized as an array of 0s, we manually set k[1] = 1
+	k[1] = 1
+	for i, j, total := 0, 2, 1; i < x; i, j, total = i+1, j+1, k[j - 1] + k[j - 2] {
+		if total >= 4000000 {
+			break
+		}
+		k = append(k, (k[j - 1] + k[j - 2]))
+	}
+	// sum is initialized to 0 using short declaration
+	sum := 0
+	for i, j := range k {
+		// if an element of index i in k is even then it gets added to the sum
+		if k[i] % 2 == 0 {
+			sum += j
+		}
+	}
+	// At the very end we return sum
+	return sum
+}
+
+// This is a simple factorial function
+func factorial(n int) int {
+	// Here the variable fact of type int is initialized to 'n'
+	// I could have declared it using short declaration 'fact := n'
+	// or even 'var fact = n' as well.
+	var fact int = n
+
+	if n == 0 {
+		return 1
+	}
+	// If n > 0, then this for loop will run and calculate the result
+	for i := n - 1; i >= 1; i = i - 1 {
+		fact = fact * i
+	}
+	// Finally, we return fact
+	return fact
+}
+
+// This function takes a string as input and returns a reverse
+// of that string.
+func reverseStr(str string) string {
+	reversedStr := ""
+	s := strings.Split(str, "")
+	for i := 0; i < len(s); i++ {
+		reversedStr = s[i] + reversedStr
+	}
+	return reversedStr
+}
